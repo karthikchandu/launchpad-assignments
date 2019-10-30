e28fb928bfed45563644cfe6ce11c35334ae6434
karthikchandu <57186207+karthikchandu@users.noreply.github.com>
2019-10-30 23:16:43 +0530

    program3

---
+++ p3.py
@@ -0,0 +1,20 @@
+test = [1,2,3,2,4,61,4,6,2,10,8]
+print("original:"+str(test))
+res = []
+for i in range(0,len(test)):
+	if test[i] == 2:
+		res.append(i)
+print(str(res))
+ 	    
+
+
+
+
+
+	
+
+
+
+
+
+		
